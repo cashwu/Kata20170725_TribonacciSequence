bc15bb027dabefa278be3b6c095ecafbadccd5cf
cash <cash@cashwu.com>
2017-07-25 15:22:03 +0800

    n=0 test case

---
+++ Kata20170725_TribonacciSequence/TribonacciSequenceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20170725_TribonacciSequence
+{
+    [TestClass]
+    public class TribonacciSequenceTests
+    {
+        [TestMethod]
+        public void input_array_0_0_1_and_n_0_return_empty()
+        {
+            var xbonacci = new Xbonacci();
+            var acutal = xbonacci.Tribonacci(new double[] {0, 0, 1}, 0);
+            CollectionAssert.AreEqual(new double[]{}, acutal);
+        }
+    }
+
+    public class Xbonacci
+    {
+        public double[] Tribonacci(double[] signature, int n)
+        {
+            return new double[]{};
+        }
+    }
+}
