088f640d997b116d893e728f8e5ed188d8f0dc76
cash <cash@cashwu.com>
2017-07-25 15:34:40 +0800

    n=3 test case

--- Kata20170725_TribonacciSequence/TribonacciSequenceTests.cs
+++ Kata20170725_TribonacciSequence/TribonacciSequenceTests.cs
@@ -8,11 +8,19 @@ namespace Kata20170725_TribonacciSequence
     public class TribonacciSequenceTests
     {
         [TestMethod]
-        public void input_array_0_0_1_and_n_0_return_empty()
+        public void input_array_0_0_1_and_n_0_return_array_0()
         {
             var xbonacci = new Xbonacci();
             var acutal = xbonacci.Tribonacci(new double[] {0, 0, 1}, 0);
-            CollectionAssert.AreEqual(new double[]{}, acutal);
+            CollectionAssert.AreEqual(new double[]{ 0 }, acutal);
+        }
+
+        [TestMethod]
+        public void input_array_0_0_1_and_n_1_return_array_0_0_1()
+        {
+            var xbonacci = new Xbonacci();
+            var acutal = xbonacci.Tribonacci(new double[] { 0, 0, 1 }, 3);
+            CollectionAssert.AreEqual(new double[] { 0, 0, 1 }, acutal);
         }
     }
 
@@ -20,7 +28,17 @@ namespace Kata20170725_TribonacciSequence
     {
         public double[] Tribonacci(double[] signature, int n)
         {
-            return new double[]{};
+            if (n == 0)
+            {
+                return new double[] { 0 };
+            }
+
+            if (n == 3)
+            {
+                return signature;
+            }
+
+            return new double[] { 0 };
         }
     }
 }
