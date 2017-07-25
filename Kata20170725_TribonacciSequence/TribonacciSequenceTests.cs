7e1846f3148b9b26d46b521d9b8214afcdae0ee3
cash <cash@cashwu.com>
2017-07-25 15:41:29 +0800

    n=4 test case

--- Kata20170725_TribonacciSequence/TribonacciSequenceTests.cs
+++ Kata20170725_TribonacciSequence/TribonacciSequenceTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170725_TribonacciSequence
@@ -14,9 +15,15 @@ namespace Kata20170725_TribonacciSequence
         }
 
         [TestMethod]
-        public void input_array_0_0_1_and_n_1_return_array_0_0_1()
+        public void input_array_0_0_1_and_n_3_return_array_0_0_1()
         {
             AssertSignatureShouldBe(new double[] { 0, 0, 1 }, 3, new double[] { 0, 0, 1 });
+        }
+
+        [TestMethod]
+        public void input_array_0_0_1_and_n_4_return_array_0_0_1_1()
+        {
+            AssertSignatureShouldBe(new double[] { 0, 0, 1 }, 4, new double[] { 0, 0, 1, 1 });
         }
 
         private static void AssertSignatureShouldBe(double[] signature, int n, double[] expected)
@@ -36,12 +43,19 @@ namespace Kata20170725_TribonacciSequence
                 return new double[] { 0 };
             }
 
+            var result = signature.ToList();
+
             if (n == 3)
             {
                 return signature;
             }
 
-            return new double[] { 0 };
+            for (int i = 0; i < n - 3; i++)
+            {
+                result.Add(result[0] + result[1] + result[2]);                
+            }
+
+            return result.ToArray();
         }
     }
 }
