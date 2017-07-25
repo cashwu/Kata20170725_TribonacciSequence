450a344c74e7a91d73c37285849fdf6ef27b4957
cash <cash@cashwu.com>
2017-07-25 15:48:54 +0800

    add code wars test case

--- Kata20170725_TribonacciSequence/TribonacciSequenceTests.cs
+++ Kata20170725_TribonacciSequence/TribonacciSequenceTests.cs
@@ -28,9 +28,21 @@ namespace Kata20170725_TribonacciSequence
         }
 
         [TestMethod]
-        public void input_array_0_0_1_and_n_5_return_array_0_0_1_1_2()
+        public void input_array_0_0_1_and_n_10_return_array_0_0_1_1_2_4_7_13_24_44()
         {
-            AssertSignatureShouldBe(new double[] { 0, 0, 1 }, 5, new double[] { 0, 0, 1, 1, 2 });
+            AssertSignatureShouldBe(new double[] { 0, 0, 1 }, 10, new double[] { 0, 0, 1, 1, 2, 4, 7, 13, 24, 44 });
+        }
+
+        [TestMethod]
+        public void input_array_0_1_1_and_n_10_return_array_0_1_1_2_4_7_13_24_44_81()
+        {
+            AssertSignatureShouldBe(new double[] { 0, 1, 1 }, 10, new double[] { 0, 1, 1, 2, 4, 7, 13, 24, 44, 81 });
+        }
+
+        [TestMethod]
+        public void input_array_1_1_1_and_n_4_return_array_1_1_1_3_5_9_17_31_57_105()
+        {
+            AssertSignatureShouldBe(new double[] { 1, 1, 1 }, 10, new double[] { 1, 1, 1, 3, 5, 9, 17, 31, 57, 105 });
         }
 
         private static void AssertSignatureShouldBe(double[] signature, int n, double[] expected)
